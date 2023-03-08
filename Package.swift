0d09af5e407336c5c42c5185f266b54353094953
Hao Yang <hao.yang@gtomato.com>
2023-03-08 13:54:12 +0800

    Bugfix and support SPM

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "VTAutoSlideView",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(name: "VTAutoSlideView", targets: ["VTAutoSlideView"]),
+    ],
+    dependencies: [],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(name: "VTAutoSlideView", dependencies: []),
+        .testTarget(name: "VTAutoSlideViewTests", dependencies: ["VTAutoSlideView"]),
+    ]
+)
