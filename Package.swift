e8176639856bc73b46f863288481128c1ef71cc4
Vito <740697612@qq.com>
2023-04-01 01:46:55 +0800

    custom spm source path (#4)
    Co-authored-by: Hao Yang <740697612@qq.com>

--- Package.swift
+++ Package.swift
@@ -12,7 +12,15 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "VTAutoSlideView", dependencies: []),
-        .testTarget(name: "VTAutoSlideViewTests", dependencies: ["VTAutoSlideView"]),
+        .target(
+            name: "VTAutoSlideView",
+            dependencies: [],
+            path: "VTAutoSlideView"
+        ),
+        .testTarget(
+            name: "VTAutoSlideViewTests",
+            dependencies: ["VTAutoSlideView"],
+            path: "VTAutoSlideViewTests"
+        ),
     ]
 )
